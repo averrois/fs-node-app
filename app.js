a79bb5190da40ffcf85a9139467e21a379b4ca18
Yassin-Aru <jocarhisouka@gmail.com>
2023-11-28 22:37:23 +0100

    Open the command file

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs = require('fs/promises');
 
 (async () => {
+    // Open the File
+    fs.open('./command.txt', 'r');
+    // Watching the file
     const watcher = fs.watch("./command.txt");
     for await (const event of watcher) {
         console.log('The File Changed');
+        // Reading the Content for the file
     }
 })()
