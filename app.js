4ea35c4f1fbbfc4da5e107fff82518cc5cd7311c
Yassin-Aru <jocarhisouka@gmail.com>
2023-11-29 14:53:12 +0100

    Setting up the size of file

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const fs = require('fs/promises');
 
 (async () => {
     // Open the File 'r' flag is for just knwing that we are only attempt to read the file.
-    const cocmmandFileHandler = await fs.open('./command.txt', 'r');
+    const commandFileHandler = await fs.open('./command.txt', 'r');
     // Watching the file
     const watcher = fs.watch("./command.txt");
     for await (const event of watcher) {
         console.log('The File Changed');
         // Reading the Content for the file
+
+        // Getting the size of the file
+        const fileSize = await commandFileHandler.stat();
+        // const content = await commandFileHandler.read();
+        // console.log(content)
+        console.log(fileSize.size);
     }
 })()
