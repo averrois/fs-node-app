228ea0c243cc704942ad7c681ebd82cbf28d08c5
Yassin-Aru <jocarhisouka@gmail.com>
2023-11-29 17:10:27 +0100

    Adding some comments

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const fs = require('fs/promises');
     // Watching the file
     const watcher = fs.watch("./command.txt");
     for await (const event of watcher) {
-        console.log('The File Changed');
-
         // Getting the size of the file
         const fileSize = (await commandFileHandler.stat()).size;
-
         // Create Buffer, Offset, Lenght, Position. Because they are and arg of read fun
+        // alloc our buffer with size of content in file
         const buff = Buffer.alloc(fileSize);
+        // offset: the location at which we want to start filling out our buff
         const offset = 0;
+        // Represet how many bytes we want to read from our content that sotresd in the buffer
+        // in this case we reading it all of it.
         const length = buff.byteLength;
+        // The position where we want to start reading file from.
         const position = 0;
 
         // Reading the Content for the file & allocating buffer size
